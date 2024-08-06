d587d14a118832de67e1e8ccf45b46d31b8eb95b
Dtbissy <isaactheedwards@gmail.com>
2024-08-05 22:45:28 -0500

    Freaking AJAX man this was not it not even alittle i was trying to write it in jquerey all the way but that was a no go and now we have this never ending page of json info but its formatted nicely-ish and im going to home and to sleep gah lee

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -22,3 +22,30 @@ $(document).ready(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      data.forEach(function (place) {
+        $('section.places').append(`
+          <article>
+            <div class="title">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest}</div>
+              <div class="number_rooms">${place.number_rooms}</div>
+              <div class="number_bathrooms">${place.number_bathrooms}</div>
+            </div>
+            <div class="description">${place.description}</div>
+          </article>
+        `);
+      });
+    }
+  });
+});
